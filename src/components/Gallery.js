d4d760af8dbc52e51c34d5c27c058ba99cc70c18
navinduk <navindu.nk@gmail.com>
2020-10-10 01:48:30 +0530

    Gallery Completed

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -6,40 +6,40 @@ import img3 from './assets/img/loft.jpg';
 const Gallery = () => {
 	return (
 		<div>
-			<div className="gradient photo-gallery">
+			<div className="photo-gallery">
 				<div className="container">
-					<div className="intro">
-						<h2 className="text-center">Lightbox Gallery</h2>
+					<div className="intro" style={{ marginTop:'3vh'}}>
+						<h2 className="text-center" style={{ fontSize: '2rem', marginBottom:'5vh'}}>Graphic Design</h2>
 					</div>
 					<div className="row photos">
-						<div className="col-sm-4 col-md-3 col-lg-2 item zoom-on-hover">
+						<div className="col-sm-6 col-md-3 col-lg-3 item zoom-on-hover">
 							<a data-lightbox="photos" href={img1}>
-								<img className="img-fluid" src={img1} />
+								<img className="img-fluid" alt="Gallery Image1" src={img1} />
 							</a>
 						</div>
-						<div className="col-sm-4 col-md-3 col-lg-2 item zoom-on-hover">
+						<div className="col-sm-6 col-md-3 col-lg-3 item zoom-on-hover">
 							<a data-lightbox="photos" href={img2}>
-								<img className="img-fluid" src={img2} />
+								<img className="img-fluid" alt="Gallery Image2" src={img2} />
 							</a>
 						</div>
-						<div className="col-sm-4 col-md-3 col-lg-2 item zoom-on-hover">
+						<div className="col-sm-6 col-md-3 col-lg-3 item zoom-on-hover">
 							<a data-lightbox="photos" href={img3}>
-								<img className="img-fluid" src={img3} />
+								<img className="img-fluid" alt="Gallery Image3" src={img3} />
 							</a>
 						</div>
-						<div className="col-sm-4 col-md-3 col-lg-2 item zoom-on-hover">
+						<div className="col-sm-6 col-md-3 col-lg-3 item zoom-on-hover">
 							<a data-lightbox="photos" href={img2}>
-								<img className="img-fluid" src={img2} />
+								<img className="img-fluid" alt="Gallery Image4" src={img2} />
 							</a>
 						</div>
-						<div className="col-sm-4 col-md-3 col-lg-2 item zoom-on-hover">
+						<div className="col-sm-6 col-md-3 col-lg-3 item zoom-on-hover">
 							<a data-lightbox="photos" href={img3}>
-								<img className="img-fluid" src={img3} />
+								<img className="img-fluid" alt="Gallery Image5" src={img3} />
 							</a>
 						</div>
-						<div className="col-sm-4 col-md-3 col-lg-2 item zoom-on-hover">
+						<div className="col-sm-6 col-md-3 col-lg-3 item zoom-on-hover">
 							<a data-lightbox="photos" href={img1}>
-								<img className="img-fluid" src={img1} />
+								<img className="img-fluid" alt="Gallery Image6" src={img1} />
 							</a>
 						</div>
 					</div>
